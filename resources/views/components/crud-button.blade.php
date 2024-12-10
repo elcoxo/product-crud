9ee712a10228ac14fc7e548f0f6edf850797a7ff
Artem Galoyan <rakkii7777@gmail.com>
2024-12-10 23:34:46 +0300

    fix: topbar crud buttons form submit

--- resources/views/components/crud-button.blade.php
+++ resources/views/components/crud-button.blade.php
@@ -8,10 +8,14 @@
         <x-primary-button href="{{ url()->current() . '/edit' }}" icon="bi-plus" variant="primary">Edit
         </x-primary-button>
     @elseif($crud === 'create')
-        <x-primary-button type="submit" icon="bi-check" variant="primary">Save</x-primary-button>
+        <x-primary-button type="submit" icon="bi-check" variant="primary" onclick="
+                           document.getElementById('store').submit();">Save
+        </x-primary-button>
         <x-primary-button href="{{url()->previous()}}" icon="bi-x" variant="danger">Cancel</x-primary-button>
     @elseif($crud === 'edit')
-        <x-primary-button type="submit" icon="bi-check" variant="primary">Save</x-primary-button>
+        <x-primary-button type="submit" icon="bi-check" variant="primary" onclick="
+                           document.getElementById('store').submit();">Save
+        </x-primary-button>
         <x-primary-button href="{{url()->previous()}}" icon="bi-x" variant="danger">Cancel</x-primary-button>
     @endif
 </div>
