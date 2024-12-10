f22442dc7b9bfe4005a1abf7e2790994a8f6669c
Artem Galoyan <rakkii7777@gmail.com>
2024-12-11 00:16:33 +0300

    feat: add authentication login and register controller logic

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -16,10 +16,25 @@ class RegisteredUserController extends BaseController
 
     public function create()
     {
+        return view('auth.register');
     }
 
     public function store(Request $request)
     {
+        // validate
+        $attributes = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5|confirmed',
+        ]);
+        // create user
+        $user = User::create($attributes);
+        // log in
+        Auth::login($user);
+        // redirect
+        return redirect(route('products.admin.categories.index'));
 
+
+        //dd(__METHOD__, request()->all());
     }
 }
--- app/Http/Controllers/Auth/SessionController.php
+++ app/Http/Controllers/Auth/SessionController.php
@@ -15,15 +15,32 @@ class SessionController extends Controller
 
     public function create()
     {
+        return view('auth.login');
     }
 
     public function store(Request $request)
     {
+        // validate
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
+        // attempt log in
+        if (!\Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect(route('products.admin.categories.index'));
     }
 
     public function destroy()
     {
-
+        \Auth::logout();
+        return redirect(route('products.admin.categories.index'));
     }
 }
